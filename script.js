876fe85221102d1c0210a84ed042b7a6f3719e6b
JAY <akinpeluoluwajuwonlo@gmail.com>
2026-09-11 15:31:14 -0400

    Add smooth scrolling for anchor links

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for anchor links (e.g., clicking "Suites" in the nav)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
